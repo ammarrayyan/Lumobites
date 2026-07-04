0d2f886d69b4062094fccbf011e7bc87d6f2397c
Ammar <ammaralrayyan999@gmail.com>
2026-07-03 22:50:12 -0400

    fix(ios): Safely configure Firebase on AppDelegate and register GoogleService-Info.plist in Xcode target

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,7 +8,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+        // Initialize Firebase safely
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
         // Override point for customization after application launch.
         return true
     }
